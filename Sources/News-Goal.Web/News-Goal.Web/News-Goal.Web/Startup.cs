10349509d5391f6d78333d1a8b416981721b0cf7
666danger <eb486535@opayq.com>
2014-11-20 18:38:31 +0400

    first commit

---
+++ Sources/News-Goal.Web/News-Goal.Web/News-Goal.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(News_Goal.Web.Startup))]
+namespace News_Goal.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
